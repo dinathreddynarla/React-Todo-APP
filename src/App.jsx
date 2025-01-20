574b9a25fbd1fb2fe7f395d1edef8e80a1d9e6ca
dinathreddynarla <reddydinath18@gmail.com>
2025-01-20 16:54:15 +0530

    reducer used

--- src/App.jsx
+++ src/App.jsx
@@ -32,3 +32,53 @@ function App() {
 }
 
 export default App
+
+// import { useEffect, useReducer, useState } from 'react'
+// import './App.css'
+// import Todo from './Components/Todo'
+// import TodoList from './Components/TodoList'
+
+
+// const reducer = (state,action)=>{
+//   switch(action.type) {
+//     case "ADD_TODO" :
+//       return [...state,action.payload] ;
+//     case "DELETE_TODO" :
+//       return state.filter((_,i)=> i !== action.payload) ;
+//     case "SET_TODO":
+//       return action.payload;
+//     default:
+//       throw new Error('Unknown action type');
+//   }
+// }
+
+
+// function App() {
+//   const [todos, dispatch] = useReducer(reducer,[]);
+
+//   useEffect(() => {
+//     const storedTodos = localStorage.getItem("todos");
+//     if (storedTodos) {
+//       dispatch({type:"SET_TODO" , payload :JSON.parse(storedTodos)})
+//     }
+//   }, []);
+
+
+//   useEffect(()=>{
+//     localStorage.setItem("todos",JSON.stringify(todos))
+//   },[todos])
+
+
+//   const handleDelete = (index) =>{
+//     dispatch({type:"DELETE_TODO" , payload:index})
+//   }
+
+//   return (
+//     <>
+//      <Todo dispatch={dispatch}/>
+//      <TodoList todos={todos} handleDelete={handleDelete} />
+//     </>
+//   )
+// }
+
+// export default App
--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -28,3 +28,34 @@ const Todo = ({setTodos , todos}) => {
 };
 
 export default Todo;
+
+// import React, { useState } from 'react';
+
+// const Todo = ({dispatch}) => {
+//     const [inputValue, setInputValue] = useState("");
+    
+
+//     const handleInput = (e) => {
+//         setInputValue(e.target.value);
+//     };
+
+//     const handleSubmit = (e) => {
+//         e.preventDefault();
+//         if (inputValue.trim()) {
+//             dispatch({type:"ADD_TODO" , payload : inputValue})
+//             setInputValue("");
+//         }
+//     };
+
+//     return (
+//         <div id="todo-container">
+//             <h1>Todo List</h1>
+//             <form onSubmit={handleSubmit}>
+//                 <input type="text" value={inputValue} onChange={handleInput} placeholder="Enter Todo item" required/>
+//                 <button type="submit">Add Todo</button>
+//             </form>
+//         </div>
+//     );
+// };
+
+// export default Todo;
